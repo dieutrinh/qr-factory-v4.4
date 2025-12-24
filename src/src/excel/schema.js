bc3f9b09a5c42d1b14b8d5c78227ebeca4aa2ca9
dieutrinh <56671269+dieutrinh@users.noreply.github.com>
2025-12-24 22:44:28 +0700

    Add files via upload

--- src/src/excel/schema.js
+++ src/src/excel/schema.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+  SCHEMA_VERSION: "4.4",
+  ALLOWED_STATUS: ["active", "disabled"],
+  SHEETS: {
+    products: "products",
+    customers: "customers",
+    staff: "staff",
+    config: "config",
+  },
+};
