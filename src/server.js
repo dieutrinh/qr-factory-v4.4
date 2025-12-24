cac40736f8d99b3bc2cc76120b2cc82f1046199a
dieutrinh <56671269+dieutrinh@users.noreply.github.com>
2025-12-24 22:53:49 +0700

    Delete src directory

--- src/server.js
+++
@@ -1,9 +0,0 @@
-\
-const express = require("express");
-
-function startServer({port, bindHost="0.0.0.0"}){
-  const app = express();
-  app.get("/health",(req,res)=>res.json({ok:true, ts:Date.now()}));
-  return app.listen(port, bindHost, ()=>console.log(`[server] http://${bindHost}:${port}`));
-}
-module.exports = { startServer };
